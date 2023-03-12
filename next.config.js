e6aa15ec14f5a02b79609007bda87bd55134b96c
Lucas <lucassoares2007@hotmail.com>
2023-03-12 17:40:27 -0300

    build: redirect mapos

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
+    reactStrictMode: true,
+    compiler: {
+        styledComponents: true,
+    },
+    async redirects() {
+        return [
+            {
+                source: "/mapos",
+                destination: "http://getsolucoes.byethost14.com/mapos/",
+                permanent: false,
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
